b1552859a8a44ab5c19df9bb21bb9498c8be5923
GrooveypenguinX <79855395+GrooveypenguinX@users.noreply.github.com>
2023-10-16 10:42:05 -0400

    Add files via upload

---
+++ PunisherBossMod/PunisherBossModPreloader/WildSpawnTypePatch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Mono.Cecil;
+
+namespace PunisherBossPreloader
+{
+
+    public static class WildSpawnTypePatch
+    {
+
+        public static IEnumerable<string> TargetDLLs { get; } = new string[]
+        {
+            "Assembly-CSharp.dll"
+        };
+
+
+        public static void Patch(ref AssemblyDefinition assembly)
+        {
+            TypeDefinition type = assembly.MainModule.GetType("EFT.WildSpawnType");
+            FieldDefinition item = new FieldDefinition("bosspunisher", FieldAttributes.Public | FieldAttributes.Static | FieldAttributes.Literal | FieldAttributes.HasDefault, type)
+            {
+                Constant = bosspunisherValue
+            };
+            type.Fields.Add(item);
+        }
+
+        public static uint bosspunisherValue = 4206927U;
+    }
+}
